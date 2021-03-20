61b98ce159b6cd2c7a045502a1e59334543b841f
dukee02 <68999242+dukee02@users.noreply.github.com>
2021-03-20 11:05:33 +0100

    Added: Possibility to predefine the city side

--- WW2-BECTI.Map_name/Rsc/Parameters.hpp
+++ WW2-BECTI.Map_name/Rsc/Parameters.hpp
@@ -637,6 +637,8 @@ class Params {
 	};
 	class CTI_TOWNS_STARTING_MODE {
 		title = "TOWNS: Starting Mode";
+		//values[] = {-1,0,1,2,3,4,5};
+		//texts[] = {"pre defined","Resistance","50% East, 50% West", "Nearby Towns", "Random Towns (25% East, 25% West, 50% Res)","Coop at East side, 'Distance' affects starting border","Coop at West side, 'Distance' affects starting border"};
 		values[] = {0,1,2,3,4,5};
 		texts[] = {"Resistance","50% East, 50% West", "Nearby Towns", "Random Towns (25% East, 25% West, 50% Res)","Coop at East side, 'Distance' affects starting border","Coop at West side, 'Distance' affects starting border"};
 		default = 0;
