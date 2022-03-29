f6d44866069933757c5cda22847a0b654f516403
dukee02 <68999242+dukee02@users.noreply.github.com>
2022-03-29 14:49:55 +0200

    Added: First Zeus implementation

--- WW2-BECTI.Map_name/Rsc/Game.hpp
+++ WW2-BECTI.Map_name/Rsc/Game.hpp
@@ -4,11 +4,11 @@ respawnDelay = 3;
 respawnDialog = false;
 
 //--- Require briefing.html to show up.
-onLoadMission = "VIO-BECTI-WW2 V2.0";
+onLoadName = "VIO-BECTI-WW2 V2.0";
 onLoadMissionTime = false;
-onLoadName = "VIO-BECTI-WW2 V2.0_220311";
+onLoadMission = "VIO-BECTI-WW2 V2.0_220318";
 
-briefingName = "VIO-BECTI-WW2 V2.0_220311";
+briefingName = "VIO-BECTI-WW2 V2.0_220318";
 
 //--- ArmA 3 Specifics
 dev = "Benny, dukee";
