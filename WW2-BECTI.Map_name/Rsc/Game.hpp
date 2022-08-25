64cf90542668724b6fa9fce7010d7edf98ad91dc
dukee02 <68999242+dukee02@users.noreply.github.com>
2022-08-25 13:52:51 +0200

    Fixed: breaks loading after map change, when only 1 save active

--- WW2-BECTI.Map_name/Rsc/Game.hpp
+++ WW2-BECTI.Map_name/Rsc/Game.hpp
@@ -6,7 +6,7 @@ respawnDialog = false;
 //--- Require briefing.html to show up.
 onLoadName = "VIO-BECTI-WW2 V2.0";
 onLoadMissionTime = false;
-onLoadMission = "VIO-BECTI-WW2 V2.0_220804";
+onLoadMission = "VIO-BECTI-WW2 V2.0_220825";
 
 briefingName = "VIO-BECTI-WW2 V2.0";
 
